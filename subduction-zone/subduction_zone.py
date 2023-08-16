8ff7fccedb7c888399d28274f26d35ffb9f3ba93
nate-sime <nsime@carnegiescience.edu>
2023-08-16 16:40:40 -0400

    updates to subduction zone code

--- subduction-zone/subduction_zone.py
+++ subduction-zone/subduction_zone.py
@@ -9,7 +9,7 @@ import ufl
 import mesh_generator
 import model
 
-model = model.SlabData()
+slab_data = model.SlabData()
 Labels = mesh_generator.Labels
 mesh, cell_tags, facet_tags = mesh_generator.generate(MPI.COMM_WORLD)
 
@@ -26,15 +26,16 @@ S = dolfinx.fem.FunctionSpace(mesh, ("CG", p_order))
 
 if mesh.comm.rank == 0:
     print(f"Velocity DoFs: "
-          f"{V.dofmap.index_map.size_global * V.dofmap.index_map_bs}")
-    print(f"Pressure DoFs: {Q.dofmap.index_map.size_global}")
-    print(f"Temperature DoFs: {S.dofmap.index_map.size_global}")
+          f"{V.dofmap.index_map.size_global * V.dofmap.index_map_bs}",
+          flush=True)
+    print(f"Pressure DoFs: {Q.dofmap.index_map.size_global}", flush=True)
+    print(f"Temperature DoFs: {S.dofmap.index_map.size_global}", flush=True)
 
 u, p, T = map(ufl.TrialFunction, (V, Q, S))
 v, q, s = map(ufl.TestFunction, (V, Q, S))
 
 uh, ph, Th = map(dolfinx.fem.Function, (V, Q, S))
-Th.interpolate(lambda x: np.full_like(x[0], model.Ts))
+Th.interpolate(lambda x: np.full_like(x[0], slab_data.Ts))
 Th.name = "T"
 uh.name = "u"
 
@@ -42,22 +43,37 @@ uh.name = "u"
 V_full = dolfinx.fem.VectorFunctionSpace(mesh, ("DG", p_order))
 uh_full = dolfinx.fem.Function(V_full, name="u_full")
 wedge_cells = cell_tags.indices[cell_tags.values == Labels.wedge]
-nm_interp_data = dolfinx.fem.create_nonmatching_meshes_interpolation_data(
+uh_to_uh_full_interp_data = \
+    dolfinx.fem.create_nonmatching_meshes_interpolation_data(
         uh_full.function_space.mesh.geometry,
         uh_full.function_space.element,
         uh.function_space.mesh._cpp_object, wedge_cells)
 
+# Set the slab velocity in the full domain velocity approximation
 slab_velocity = 2**(-0.5) * np.array([1.0, -1.0], dtype=PETSc.ScalarType)
 uh_full.interpolate(lambda x: np.stack([np.full_like(x[i], slab_velocity[i])
                         for i in range(mesh.geometry.dim)]),
                     cells=cell_tags.indices[cell_tags.values == Labels.slab])
 
+# Interpolate the wedge temperature component for the Stokes problem
+S_wedge = dolfinx.fem.FunctionSpace(wedge_mesh, ("CG", p_order))
+Th_wedge = dolfinx.fem.Function(S_wedge, name="T_wedge")
+Th_to_Th_wedge_interp_data = \
+    dolfinx.fem.create_nonmatching_meshes_interpolation_data(
+        Th_wedge.function_space.mesh._cpp_object,
+        Th_wedge.function_space.element,
+        Th.function_space.mesh._cpp_object)
+Th_wedge.interpolate(Th, nmm_interpolation_data=Th_to_Th_wedge_interp_data)
+Th_wedge.x.scatter_forward()
+
+# Converted from the implementation by C. R. Wilson
+# https://github.com/TerraFERMA/benchmarks/blob/master/benchmarks/vankeken/1a/vankeken.tfml#L233-L250
 def gkb_wedge_flow_(X):
     from numpy import cos, sin, arctan
-    plate_thickness = model.plate_thickness
+    plate_thickness = slab_data.plate_thickness
     depth = -plate_thickness - X[1]
     xdist = X[0] - plate_thickness
-    xdist[np.isclose(xdist, 0.0)] = 0.000000000000001
+    xdist[np.isclose(xdist, 0.0)] = np.finfo(np.float64).eps
     values = np.zeros((2, X.shape[1]), dtype=np.double)
     alfa = arctan(1.0)
     theta = arctan(depth / xdist)
@@ -77,25 +93,30 @@ def gkb_wedge_flow_(X):
 uh_full.interpolate(gkb_wedge_flow_, cells=wedge_cells)
 uh_full.x.scatter_forward()
 
+# Useful initial guess for strainrate dependent viscosities
+uh.interpolate(gkb_wedge_flow_)
+uh.x.scatter_forward()
+
 # -- Stokes system
-solve_flow = True
+solve_flow = False
 if solve_flow:
-    def eta(u, T):
-        return 1
+    # eta = model.create_viscosity_1()
+    # eta = model.create_viscosity_2a(wedge_mesh)
+    eta = model.create_viscosity_2b(wedge_mesh, slab_data)
+
+    def sigma(u, u0, T):
+        return 2 * eta(u0, T) * ufl.sym(ufl.grad(u))
 
 
-    def sigma(u, T):
-        return 2 * eta(u, T) * ufl.sym(ufl.grad(u))
-
-
-    a_u00 = ufl.inner(sigma(u, T), ufl.sym(ufl.grad(v))) * ufl.dx
+    a_u00 = ufl.inner(sigma(u, uh, Th_wedge), ufl.sym(ufl.grad(v))) * ufl.dx
     a_u01 = - ufl.inner(p, ufl.div(v)) * ufl.dx
     a_u10 = - ufl.inner(q, ufl.div(u)) * ufl.dx
     a_u = dolfinx.fem.form(
         [[a_u00, a_u01],
          [a_u10, None]])
+    a_p11 = dolfinx.fem.form(-eta(uh, Th_wedge)**-1 * ufl.inner(p, q) * ufl.dx)
     a_p = [[a_u[0][0], a_u[0][1]],
-           [a_u[1][0], dolfinx.fem.form(-eta(u, T)**-1 * ufl.inner(p, q) * ufl.dx)]]
+           [a_u[1][0], a_p11]]
 
     f_u = dolfinx.fem.Constant(wedge_mesh, [0.0] * mesh.geometry.dim)
     f_p = dolfinx.fem.Constant(wedge_mesh, 0.0)
@@ -114,16 +135,28 @@ if solve_flow:
         slab_velocity, dolfinx.fem.locate_dofs_topological(
             V, mesh.topology.dim-1, facets), V)
 
-    # The slab plate BC goes last such that all dofs on the overriding plate
-    # have zero flow
+    # The plate BC goes last such that all dofs on the overriding plate
+    # have priority and therefore zero flow
     bcs_u = [bc_slab, bc_plate]
 
     # -- Stokes linear system and solver
-    A_u = dolfinx.fem.petsc.assemble_matrix_block(a_u, bcs=bcs_u)
-    A_u.assemble()
-    P_u = dolfinx.fem.petsc.assemble_matrix_block(a_p, bcs=bcs_u)
-    P_u.assemble()
-    b_u = dolfinx.fem.petsc.assemble_vector_block(L_u, a_u, bcs=bcs_u)
+    A_u = dolfinx.fem.petsc.create_matrix_block(a_u)
+    P_u = dolfinx.fem.petsc.create_matrix_block(a_p)
+    b_u = dolfinx.fem.petsc.create_vector_block(L_u)
+
+    def assemble_stokes_system():
+        A_u.zeroEntries()
+        dolfinx.fem.petsc.assemble_matrix_block(A_u, a_u, bcs=bcs_u)
+        A_u.assemble()
+
+        P_u.zeroEntries()
+        dolfinx.fem.petsc.assemble_matrix_block(P_u, a_p, bcs=bcs_u)
+        P_u.assemble()
+
+        with b_u.localForm() as b_u_local:
+            b_u_local.set(0.0)
+        dolfinx.fem.petsc.assemble_vector_block(b_u, L_u, a_u, bcs=bcs_u)
+
 
     ksp_u = PETSc.KSP().create(mesh.comm)
     ksp_u.setOperators(A_u)
@@ -133,23 +166,19 @@ if solve_flow:
     pc_u.setFactorSolverType("mumps")
 
     x_u = A_u.createVecLeft()
-    ksp_u.solve(b_u, x_u)
-    offset = V.dofmap.index_map.size_local * V.dofmap.index_map_bs
-    uh.x.array[:offset] = x_u.array_r[:offset]
-    uh.x.scatter_forward()
+    def solve_stokes_system():
+        ksp_u.solve(b_u, x_u)
+        offset = V.dofmap.index_map.size_local * V.dofmap.index_map_bs
+        uh.x.array[:offset] = x_u.array_r[:offset]
+        uh.x.scatter_forward()
 
-    # -- Interpolate velocity approximation into full geometry
-    uh_full.interpolate(
-        uh, cells=wedge_cells,
-        nmm_interpolation_data=nm_interp_data)
-    uh_full.x.scatter_forward()
 
 # -- Heat system
 a_T = dolfinx.fem.form(
-    ufl.inner(model.k_prime * ufl.grad(T), ufl.grad(s)) * ufl.dx
-    + ufl.inner(ufl.dot(model.rho * model.cp * uh_full, ufl.grad(T)), s) * ufl.dx
+    ufl.inner(slab_data.k_prime * ufl.grad(T), ufl.grad(s)) * ufl.dx
+    + ufl.inner(ufl.dot(slab_data.rho * slab_data.cp * uh_full, ufl.grad(T)), s) * ufl.dx
 )
-Q_prime_constant = dolfinx.fem.Constant(mesh, model.Q_prime)
+Q_prime_constant = dolfinx.fem.Constant(mesh, slab_data.Q_prime)
 L_T = dolfinx.fem.form(ufl.inner(Q_prime_constant, s) * ufl.dx)
 
 # -- Heat BCs
@@ -158,20 +187,20 @@ def depth(x):
 
 def slab_inet_temp_(x):
     vals = np.zeros((1, x.shape[1]))
-    Ts = model.Ts
-    Twedge_in = model.Twedge_in
-    z_scale = model.h_r
-    kappa = model.kappa_slab
-    t50 = model.t50
+    Ts = slab_data.Ts
+    Twedge_in = slab_data.Twedge_in
+    z_scale = slab_data.h_r
+    kappa = slab_data.kappa_slab
+    t50 = slab_data.t50
     vals[0] = Ts + (Twedge_in - Ts) * scipy.special.erf(
         depth(x) * z_scale / (2.0 * np.sqrt(kappa * t50)))
     return vals
 
 def overriding_side_temp_(x):
     vals = np.zeros((1, x.shape[1]))
-    Ts = model.Ts
-    T0 = model.Twedge_in
-    Zplate = model.plate_thickness
+    Ts = slab_data.Ts
+    T0 = slab_data.Twedge_in
+    Zplate = slab_data.plate_thickness
     vals[0] = np.minimum(Ts - (T0 - Ts) / Zplate * (-depth(x)), T0)
     return vals
 
@@ -197,12 +226,20 @@ bc_overriding = dolfinx.fem.dirichletbc(
 bcs_T = [bc_inlet, bc_overriding]
 
 # -- Heat linear system and solver
-A_T = dolfinx.fem.petsc.assemble_matrix(a_T, bcs=bcs_T)
-A_T.assemble()
-b_T = dolfinx.fem.petsc.assemble_vector(L_T)
-dolfinx.fem.petsc.apply_lifting(b_T, [a_T], bcs=[bcs_T])
-b_T.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
-dolfinx.fem.petsc.set_bc(b_T, bcs_T)
+A_T = dolfinx.fem.petsc.create_matrix(a_T)
+b_T = dolfinx.fem.petsc.create_vector(L_T)
+
+def assemble_temperature_system():
+    A_T.zeroEntries()
+    dolfinx.fem.petsc.assemble_matrix(A_T, a_T, bcs=bcs_T)
+    A_T.assemble()
+
+    with b_T.localForm() as b_T_local:
+        b_T_local.set(0.0)
+    dolfinx.fem.petsc.assemble_vector(b_T, L_T)
+    dolfinx.fem.petsc.apply_lifting(b_T, [a_T], bcs=[bcs_T])
+    b_T.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
+    dolfinx.fem.petsc.set_bc(b_T, bcs_T)
 
 ksp_T = PETSc.KSP().create(mesh.comm)
 ksp_T.setOperators(A_T)
@@ -211,9 +248,45 @@ pc_T = ksp_T.getPC()
 pc_T.setType("lu")
 pc_T.setFactorSolverType("mumps")
 
-ksp_T.solve(b_T, Th.vector)
-Th.x.scatter_forward()
+def solve_temperature_system():
+    ksp_T.solve(b_T, Th.vector)
+    Th.x.scatter_forward()
 
+# Compare temperature difference between Picard iterations
+Th0 = dolfinx.fem.Function(Th.function_space)
+Th0.vector.array = Th.vector.array_r
+Th0.x.scatter_forward()
+
+for picard_it in range(max_picard_its := 20):
+    # Solve Stokes and interpolate velocity approximation into full geometry
+    if solve_flow:
+        assemble_stokes_system()
+        solve_stokes_system()
+        uh_full.interpolate(
+            uh, cells=wedge_cells,
+            nmm_interpolation_data=uh_to_uh_full_interp_data)
+        uh_full.x.scatter_forward()
+
+    # Solve temperature
+    assemble_temperature_system()
+    solve_temperature_system()
+
+    # Check convergence
+    Th_l2 = Th.vector.norm(PETSc.NormType.NORM_2)
+    T_diff = (Th.vector - Th0.vector).norm(PETSc.NormType.NORM_2) / Th_l2
+
+    if mesh.comm.rank == 0:
+        print(f"Picard it {picard_it: {int(np.ceil(np.log10(max_picard_its)))}d}: "
+              f"T_diff = {T_diff:.3e} ", flush=True)
+
+    if T_diff < (picard_tol := 1e-6):
+        break
+
+    # Interpolate temperature approximation into wedge geometry
+    Th_wedge.interpolate(Th, nmm_interpolation_data=Th_to_Th_wedge_interp_data)
+    Th_wedge.x.scatter_forward()
+    Th0.vector.array = Th.vector.array_r
+    Th0.x.scatter_forward()
 
 tree = dolfinx.geometry.bb_tree(mesh, mesh.geometry.dim)
 points6060 = np.array([60.0, -60.0, 0], np.float64)
@@ -224,25 +297,17 @@ cell_collided = dolfinx.geometry.compute_colliding_cells(
 T_6060 = None
 if len(cell_collided) > 0:
     T_6060 = Th.eval(
-        points6060, cell_collided[0])[0] - model.Ts
+        points6060, cell_collided[0])[0] - slab_data.Ts
 T_6060 = mesh.comm.gather(T_6060, root=0)
 
 if mesh.comm.rank == 0:
-    print(f"T_6060 = "
-              f"{[T_val for T_val in T_6060 if T_val is not None]}")
+    print(f"T_6060 = {[T_val for T_val in T_6060 if T_val is not None]}",
+          flush=True)
 
 
 with dolfinx.io.VTXWriter(mesh.comm, "temperature.bp", Th, "bp4") as f:
     f.write(0.0)
-with dolfinx.io.VTXWriter(mesh.comm, "velocity.bp", uh, "bp4") as f:
+with dolfinx.io.VTXWriter(mesh.comm, "velocity_wedge.bp", uh, "bp4") as f:
     f.write(0.0)
-with dolfinx.io.VTXWriter(mesh.comm, "velocity_dg.bp", uh_full, "bp4") as f:
+with dolfinx.io.VTXWriter(mesh.comm, "velocity.bp", uh_full, "bp4") as f:
     f.write(0.0)
-
-# import pyvista
-# import febug
-# plotter = pyvista.Plotter(shape=(1, 2))
-# plotter.subplot(0, 0)
-# febug.plot_function(uh, plotter=plotter)
-# plotter.subplot(0, 1)
-# febug.plot_function(Th, plotter=plotter).show()
